82406ea472bb2c83987b20be1e1e3d4828b4d7bb
HKAB <khoahockithuatmc@gmail.com>
2025-04-03 10:16:19 +0700

    update dockerfile and run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM nvcr.io/nvidia/tritonserver:24.04-vllm-python-py3
+FROM nvcr.io/nvidia/pytorch:24.05-py3
 
 RUN apt-get update && apt-get install ffmpeg -y
 RUN pip install yt-dlp
 
 # Prevent pyannote-audio install higher version of torch
-RUN pip install insanely-fast-whisper torch==2.1.2 torchaudio==2.1.2 openai
+RUN pip install insanely-fast-whisper openai
 
-RUN cd /workspace && git clone https://github.com/HKAB/khoplenh-summarizer.git
+# RUN cd /workspace && git clone https://github.com/HKAB/khoplenh-summarizer.git
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker build -f Dockerfile -t tritonserver-vllm-whisper:latest .
+docker build -f Dockerfile -t truongnp_whisper_openai:latest .
 docker stop khoplenh-summarizer
 docker container rm khoplenh-summarizer
-docker run --gpus all -it --net=host --name khoplenh-summarizer --shm-size=1G --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace tritonserver-vllm-whisper:latest bash
+docker run --gpus all -it --net=host --name khoplenh-summarizer --shm-size=1G --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace truongnp_whisper_openai:latest bash
